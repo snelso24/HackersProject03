acaabca97391c19cf9bb204d1af8e58842af5569
marcellena <112984768+marcellena@users.noreply.github.com>
2024-12-11 16:49:00 -0500

    Add files via upload

---
+++ website.js
@@ -0,0 +1,10 @@
+function github(){
+
+window.open("https://github.com/snelso24/Project03/tree/main/PeerReview");
+
+}
+function googleFlags(){
+
+  window.open("chrome://flags/#optimization-guide-on-device-model");
+  
+  }
