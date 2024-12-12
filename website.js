2d72dcd45d01de7ba14e432f83395ec8381e7277
marcellena <112984768+marcellena@users.noreply.github.com>
2024-12-12 06:47:26 -0500

    Add files via upload

--- website.js
+++ website.js
@@ -1,10 +1,12 @@
 function github(){
 
-window.open("https://github.com/snelso24/Project03/tree/main/PeerReview");
+window.open("https://github.com/snelso24/HackersProject03/tree/main/ProjectPeerReview");
 
 }
 function googleFlags(){
 
   window.open("chrome://flags/#optimization-guide-on-device-model");
   
-  }
+}
+const img = document.createElement('img');
+img.src
